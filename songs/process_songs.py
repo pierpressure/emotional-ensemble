d2dc7c32bdff11ad22caaa0c6e0de9b09beb7722
Pierre Karashchuk <krchtchk@gmail.com>
2016-08-07 17:56:50 -0700

    process songs with equal volume

--- songs/process_songs.py
+++ songs/process_songs.py
@@ -1,12 +1,39 @@
 import csv
 import os
+import re
+import os.path
 
-with open('songs.csv', 'rb') as csvfile:
+pat_db = re.compile(r'([+\-]?\d+\.\d+) dB')
+
+with open('songs.csv', 'r') as csvfile:
     songreader = csv.reader(csvfile, delimiter=',')
     for row in songreader:
         fname, start_time = row
-        new_name = os.path.splitext(fname)[0] + " Trimmed.mp3"
+        fname = fname.strip()
+        start_time = start_time.strip()
+
+        fname, start_time = row
+        print(fname)
+
+        new_name = os.path.splitext(fname)[0] + " Trimmed.ogg"
+        new_name_volume = os.path.splitext(fname)[0] + " Trimmed Volume.ogg"
+
+        if os.path.exists(new_name_volume):
+            continue
+
         command = "ffmpeg -y -loglevel panic -i '{}' -ss {} -t 10 '{}'".format(
-                fname, start_time.strip(), new_name)
-        print(command)
+            fname, start_time, new_name)
+        # print(command)
+        os.system(command)
+
+        cmd = "ffmpeg -i '{}'  -af 'volumedetect' -f null /dev/null 2>&1 | grep max_volume".format(new_name)
+        output = os.popen(cmd).read()
+        groups = pat_db.search(output).groups()
+        mean_volume = float(groups[0])
+        diff = -5 - mean_volume
+
+
+        command = "ffmpeg -y -loglevel panic -i '{}'  -af 'volume={:.1f}dB' '{}'".format(
+            new_name, diff, new_name_volume)
+        # print(command)
         os.system(command)
